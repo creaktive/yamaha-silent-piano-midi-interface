4a1603af3028ab2844084e99a697b28782ae4597
Stanislaw Pusep <creaktive@gmail.com>
2025-10-05 11:53:47 +0200

    keep soft pedal pressed during boot to enable USB drive

--- boot.py
+++ boot.py
@@ -1,3 +1,6 @@
+import board
+import digitalio
+import storage
 import usb_cdc, usb_hid, usb_midi
 
 usb_cdc.disable()
@@ -5,3 +8,9 @@ usb_cdc.enable(console=True, data=False)
 
 usb_hid.disable()
 usb_midi.enable()
+
+# keep soft pedal pressed during boot to enable USB drive
+soft = digitalio.DigitalInOut(board.GP0)
+soft.switch_to_input(pull=digitalio.Pull.UP)
+if not soft.value:
+    storage.disable_usb_drive()

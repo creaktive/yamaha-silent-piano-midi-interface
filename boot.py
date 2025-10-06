97f48d17adb95fa6d370fdaa8e178e63f61feaeb
Stanislaw Pusep <creaktive@gmail.com>
2025-10-06 07:29:15 +0200

    debug mode

--- boot.py
+++ boot.py
@@ -4,13 +4,13 @@ import storage
 import usb_cdc, usb_hid, usb_midi
 
 usb_cdc.disable()
-usb_cdc.enable(console=True, data=False)
-
 usb_hid.disable()
 usb_midi.enable()
 
-# keep soft pedal pressed during boot to enable USB drive
+# keep soft pedal pressed during boot to enable USB drive & debug console
 soft = digitalio.DigitalInOut(board.GP0)
 soft.switch_to_input(pull=digitalio.Pull.UP)
-if not soft.value:
+if soft.value:
+    usb_cdc.enable(console=True, data=False)
+else:
     storage.disable_usb_drive()
